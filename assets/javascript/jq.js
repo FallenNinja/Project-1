ecc9a5e71e0da9ea0f8a38606ead119552e74f64
Edwin <edwin.martinez1994@yahoo.com>
2018-10-11 08:03:35 -0500

    done

---
+++ assets/javascript/jq.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  console.log('jquery ready');
+  
+  $('#ham').click(function(){
+      var panel = $("#directionsPanel");
+      console.log("ham button click");
+      
+      if(!panel.hasClass('disabled')) {//if not disabled
+          panel.toggleClass('hidden');
+      }
+      
+//        $("#directionsPanel").animate({
+//            left: '0px'
+//        }, 200);
+  });
+});
